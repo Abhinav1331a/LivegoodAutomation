849425a433039ee5ef782dbb78fcac3e3843cc02
Abhinav1331a <abhinava465@gmail.com>
2023-08-24 06:27:15 -0300

    ..

--- LiveGoodAutomationApp/dashboard.py
+++ LiveGoodAutomationApp/dashboard.py
@@ -134,6 +134,9 @@ def detailedstats():
 
                 # Create Options
                 options = Options()
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--disable-dev-shm-usage")
                 options.add_argument("--headless")
                 options.add_argument("--window-size=1920,1080")
 
